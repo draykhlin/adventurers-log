83329b78455457f2e1fbf1f670d49ddf9a48675d
Dan Raykhlin <draykhlin@gmail.com>
2023-04-25 12:33:30 -0400

    added qty state to Item component

--- frontend/src/components/Item.js
+++ frontend/src/components/Item.js
@@ -1,10 +1,20 @@
 import { FaTimes } from 'react-icons/fa'
+import { useState, useEffect } from 'react'
 
 const Item = ({ item, onDelete }) => {
+   const [newQty, setNewQty] = useState(item.qty)
+   
+   useEffect(() => {
+      
+   })
+
    return (
       <li key={item._id} className="inventoryItem">
          <span>{item.name}</span><br></br>
-         <span>Quantity: {item.qty}</span>
+         {/* <span>Quantity: {item.qty}</span> */}
+
+         <input type="number" value={newQty} onChange={updateQty} />
+
          <FaTimes onClick={() => onDelete(item._id)} />
       </li>
    )
