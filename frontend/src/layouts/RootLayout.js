8b6f32254cfc636244c930f59f8fa11460f755df
Dan Raykhlin <draykhlin@gmail.com>
2023-05-31 14:08:16 -0400

    added container classes and styling

--- frontend/src/layouts/RootLayout.js
+++ frontend/src/layouts/RootLayout.js
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function RootLayout() {
    return (
       <div className="root-layout">
-         <header>
+         <header className="container-fluid">
             {/* <div className="logo">
                <FontAwesomeIcon icon="fa-solid fa-book" className="icon" />
                <span className="logo-text">Adventurer's Log</span>
             </div> */}
-            <nav>
+            <nav className="container">
                <NavLink to="/inventory">Inventory</NavLink>
                <NavLink to="/spells">Spells</NavLink>
             </nav>
