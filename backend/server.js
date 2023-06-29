2eb579ddb83b18b8a0ad0a5b8b777adec0214671
Dan Raykhlin <draykhlin@gmail.com>
2023-06-29 13:48:07 -0400

    fixed bug: req.logIn is not a function

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const app = express()
 const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
@@ -10,11 +11,45 @@ const authRoutes = require('./routes/auth')
 const inventoryRoutes = require('./routes/inventory')
 const gpRoutes = require('./routes/gp')
 const spellsRoutes = require('./routes/spells')
-
 require('dotenv').config({path: './config/.env'})
 
 // Passport config
 require('./config/passport')
+
+
+// middleware
+app.use(express.static('frontend/public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+// app.use('/api/Auth', require('./Auth/Route'))
+
+app.use(flash())
+
+// sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    // secret: env.get("SESSION_SECRET"),
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI
+    }),
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/inventory', inventoryRoutes)
+app.use('/api/gp', gpRoutes)
+app.use('/api/spells', spellsRoutes)
+
+
+
 
 // connectDB()
 mongoose.connect(process.env.MONGO_URI)
@@ -27,38 +62,3 @@ mongoose.connect(process.env.MONGO_URI)
  .catch((err) => {
      console.log(err)
  })
-
-
-// express
-const app = express()
-
-// middleware
-app.use(express.static('frontend/public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-// app.use('/api/Auth', require('./Auth/Route'))
-
-app.use(flash())
-
-// routes
-app.use('/api/auth', authRoutes)
-app.use('/api/inventory', inventoryRoutes)
-app.use('/api/gp', gpRoutes)
-app.use('/api/spells', spellsRoutes)
-
-// sessions
-app.use(
-    session({
-      secret: 'keyboard cat',
-      // secret: env.get("SESSION_SECRET"),
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: process.env.MONGO_URI
-      }),
-    })
-  )
-
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
