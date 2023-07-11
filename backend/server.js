c5836466f028fac0321a2998f2ca377bb0faba72
Dan Raykhlin <draykhlin@gmail.com>
2023-07-10 21:08:43 -0400

    router testing

--- backend/server.js
+++ backend/server.js
@@ -52,7 +52,7 @@ app.use('/api/spells', spellsRoutes)
 
 
 app.use(express.static(path.join(__dirname, 'build')));
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
