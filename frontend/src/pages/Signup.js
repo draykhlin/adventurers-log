0e87fddf86f168c44709c0ee77017e8a5cfd4837
Dan Raykhlin <draykhlin@gmail.com>
2023-07-13 15:51:52 -0400

    added title to signup form

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,16 +1,17 @@
 const Signup = () => {
    return (
       <>
-         <div className="hero">
-            <div className="card">
-               <form className="signup" action="/api/auth/signup" method="POST">
-                  <h2>Sign Up</h2>
-                  <input type="email" name="email" placeholder="Email" required />
-                  <input type="password" name="password" placeholder="Password" required />
-                  <button type="submit">Sign up</button>
-               </form>
-            </div>
+      <div className="hero">
+         <h1>Adventurer's Log</h1>
+         <FontAwesomeIcon icon={faDiceD20} className="d20-icon" />
+         <div className="card">
+            <form className="signup" action="/api/auth/signup" method="POST">
+               <input type="email" name="email" placeholder="Email" required />
+               <input type="password" name="password" placeholder="Password" required />
+               <button type="submit">Sign up</button>
+            </form>
          </div>
+      </div>
       </>
    )
 }
