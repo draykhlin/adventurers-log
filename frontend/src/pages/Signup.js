4b6d24a2da480902a81a67717fd3086b6bdc7615
Dan Raykhlin <draykhlin@gmail.com>
2023-07-13 19:05:15 -0400

    updated signup url

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -8,7 +8,7 @@ const Signup = () => {
          <h1>Adventurer's Log</h1>
          <FontAwesomeIcon icon={faDiceD20} className="d20-icon" />
          <div className="card">
-            <form className="signup" action="/api/auth/signup" method="POST">
+            <form className="signup" action="https://adventurers-log-server.onrender.com/api/auth/signup" method="POST">
                <input type="email" name="email" placeholder="Email" required />
                <input type="password" name="password" placeholder="Password" required />
                <button type="submit">Sign up</button>
