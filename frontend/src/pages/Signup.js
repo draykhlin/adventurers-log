43e613e2174453543d76061c77d0b41ad109e189
Dan Raykhlin <draykhlin@gmail.com>
2023-07-13 19:36:27 -0400

    fixing navigate on signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -2,8 +2,8 @@ import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiceD20 } from '@fortawesome/free-solid-svg-icons'
 
+const navigate = useNavigate()
 const handleSubmit = () => {
-   const navigate = useNavigate()
    navigate('/inventory')
 }
 
