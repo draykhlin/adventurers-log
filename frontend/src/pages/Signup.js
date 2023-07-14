a81589c7e5d86117b07cef032085d2101f3c7c65
Dan Raykhlin <draykhlin@gmail.com>
2023-07-13 20:44:02 -0400

    added useState import to signup

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faDiceD20 } from '@fortawesome/free-solid-svg-icons'
