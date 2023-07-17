cea6f2b4c415955c02b5ca9b4a46641c870a92b3
Dan Raykhlin <draykhlin@gmail.com>
2023-07-17 16:08:31 -0400

    logout testing

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -49,8 +49,9 @@ exports.logout = (req, res) => {
   })
   req.session.destroy((err) => {
     if (err) console.log('Error : Failed to destroy the session during logout.', err)
-    req.user = null
+    // req.user = null
     res.clearCookie('connect.sid')
+    res.send('Logged out')
     // res.redirect('/')
   })
 }
