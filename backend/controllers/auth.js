8fdd1b9df999b88082a25af0a6ad0f95eb120b2d
Dan Raykhlin <draykhlin@gmail.com>
2023-07-17 15:08:03 -0400

    signup testing

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -103,7 +103,7 @@ exports.postSignup = (req, res, next) => {
     //   return res.redirect('https://adventurers-log.onrender.com/inventory/signup')
     // }
     user.save((err) => {
-      // res.send('Saving user')
+      res.send('Saving user')
       if (err) { return next(err) }
       req.logIn(user, (err) => {
         if (err) {
